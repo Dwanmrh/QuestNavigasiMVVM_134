36338c1d627bb270538d1b1b52b98e7dde85d32a
RidwanC134 <ridwanhh10@gmail.com>
2024-11-21 22:37:07 +0700

    Mendefinisikan fungsi FormulirView dengan parameter untuk layout, list jenis kelamin, dan callback saat tombol simpan di klik.

--- app/src/main/java/com/dwan/navigasi_mvvm/ui/view/FormulirView.kt
+++ app/src/main/java/com/dwan/navigasi_mvvm/ui/view/FormulirView.kt
@@ -1,10 +1,13 @@
 package com.dwan.navigasi_mvvm.ui.view
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
 fun FormulirView(
-
+    modifier: Modifier = Modifier,
+    listJK: List<String>,
+    onSubmitClicked: (MutableList<String>) -> Unit
 ) {
 
 }
