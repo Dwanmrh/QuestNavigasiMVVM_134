b4ba1a11c36611b2738fb0be366688c0889ead7c
RidwanC134 <ridwanhh10@gmail.com>
2024-11-21 22:56:53 +0700

    Membuat column, row, dan text untuk menampilkan data. Menampilkan parameter param, :, dan argu.

--- app/src/main/java/com/dwan/navigasi_mvvm/ui/view/TampilDataView.kt
+++ app/src/main/java/com/dwan/navigasi_mvvm/ui/view/TampilDataView.kt
@@ -1,11 +1,16 @@
 package com.dwan.navigasi_mvvm.ui.view
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.dwan.navigasi_mvvm.model.DataSiswa
 
@@ -28,5 +33,18 @@ fun TampilData(
     param: String,
     argu: String
 ) {
-
+    Column(
+        modifier = Modifier.padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(text = param, modifier = Modifier.weight(0.8f))
+            Text(text = ": ", modifier = Modifier.weight(0.2f))
+            Text(text = argu, modifier = Modifier.weight(2f),textAlign = TextAlign.Center)
+        }
+    }
 }
