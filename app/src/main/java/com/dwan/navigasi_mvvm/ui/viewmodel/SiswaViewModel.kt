56e459ef0ef862265bb93fdb117e6adcda1893f1
RidwanC134 <ridwanhh10@gmail.com>
2024-11-21 23:07:53 +0700

    Membuat gungsi untuk menyimpan data siswa dengan parameter list string, Memperbarui nilai _statusUI menggunakan metode update, Membuat salinan objek DataSiswa dengan data baru dari list input.

--- app/src/main/java/com/dwan/navigasi_mvvm/ui/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/dwan/navigasi_mvvm/ui/viewmodel/SiswaViewModel.kt
@@ -5,6 +5,7 @@ import com.dwan.navigasi_mvvm.model.DataSiswa
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class SiswaViewModel : ViewModel {
 
@@ -13,4 +14,17 @@ class SiswaViewModel : ViewModel {
 
     // Response
     val statusUI: StateFlow<DataSiswa> = _statusUI.asStateFlow()
+
+    fun saveDataSiswa(ls: List<String>) {
+        _statusUI.update { statusSaatIni ->
+            statusSaatIni.copy(
+                nama = ls[0],
+                nim = ls[1],
+                gender = ls[2],
+                email = ls[3],
+                alamat = ls[4],
+                notelp = ls[5]
+            )
+        }
+    }
 }
