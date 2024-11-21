96ab5cd0d1234278c477d85540ce85199d8a1fb5
RidwanC134 <ridwanhh10@gmail.com>
2024-11-21 23:12:09 +0700

    Mendeklarasi fungsi untuk kontrol navigasi, Parameter ViewModel dengan default instance menggunakan viewModel, Parameter NavHostController dengan default menggunakan rememberNavController.

--- app/src/main/java/com/dwan/navigasi_mvvm/NavigationControl.kt
+++ app/src/main/java/com/dwan/navigasi_mvvm/NavigationControl.kt
@@ -1,10 +1,23 @@
 package com.dwan.navigasi_mvvm
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.dwan.navigasi_mvvm.ui.viewmodel.SiswaViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+
 
 @Composable
 fun NavigationControl(
 
 ) {
+    @Composable
+    fun NavigationControl(
+        modifier: Modifier = Modifier,
+        viewModel: SiswaViewModel = viewModel(),
+        navHost: NavHostController = rememberNavController()
+    ) {
 
+    }
 }
