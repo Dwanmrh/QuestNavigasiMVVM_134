903272b9873030c0843e784d7a098055932d5a2c
RidwanC134 <ridwanhh10@gmail.com>
2024-11-21 23:18:58 +0700

    Mendefinisikan rute navigasi untuk halaman formulir, Mengakses konteks lokal untuk mengambil resources, Mempersiapkan data jenis kelamin dengan mentransformasi ID menjadi string, Mempersiapkan pemanggilan FormulirView dengan data yang diperlukan.

--- app/src/main/java/com/dwan/navigasi_mvvm/NavigationControl.kt
+++ app/src/main/java/com/dwan/navigasi_mvvm/NavigationControl.kt
@@ -4,11 +4,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.dwan.navigasi_mvvm.ui.viewmodel.SiswaViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.dwan.navigasi_mvvm.model.JenisKelamin.JenisK
+import com.dwan.navigasi_mvvm.ui.view.FormulirView
 
 enum class Halaman {
     FORMULIR,
@@ -31,7 +35,15 @@ fun NavigationControl(
             navController = navHost,
             startDestination = Halaman.FORMULIR.name
         ) {
+            composable(
+                route = Halaman.FORMULIR.name
+            ) {
+                val context = LocalContext.current
 
+                FormulirView(
+                    modifier = modifier,
+                    listJK = JenisK.map { id ->
+                        context.getString(id)
         }
     }
 }
