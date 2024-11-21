612999a922f14781ef28e2227be696d66fc4d53a
RidwanC134 <ridwanhh10@gmail.com>
2024-11-21 22:30:17 +0700

    Mendeklarasikan data class bernama DataSiswa dan membuat val untuk nama, nim, gender, email, alamat, dan notelp bertipe string dengan default value kosong

--- app/src/main/java/com/dwan/navigasi_mvvm/model/DataSiswa.kt
+++ app/src/main/java/com/dwan/navigasi_mvvm/model/DataSiswa.kt
@@ -1,5 +1,11 @@
 package com.dwan.navigasi_mvvm.model
 
 data class DataSiswa(
-
+    val nama: String = "",
+    val nim: String = "",
+    val gender: String = "",
+    val email: String = "",
+    val alamat: String = "",
+    val notelp: String = "",
 )
+
