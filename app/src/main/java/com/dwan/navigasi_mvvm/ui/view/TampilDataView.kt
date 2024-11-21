6bcc72226aac419869d934ea43289f8ab9a07520
RidwanC134 <ridwanhh10@gmail.com>
2024-11-21 22:53:08 +0700

    Membuat column untuk funsi TampilData dan button kembali.

--- app/src/main/java/com/dwan/navigasi_mvvm/ui/view/TampilDataView.kt
+++ app/src/main/java/com/dwan/navigasi_mvvm/ui/view/TampilDataView.kt
@@ -1,7 +1,12 @@
 package com.dwan.navigasi_mvvm.ui.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.dwan.navigasi_mvvm.model.DataSiswa
 
 @Composable
@@ -10,5 +15,10 @@ fun TampilDataView(
     uiState: DataSiswa,
     onBackButton: () -> Unit
 ) {
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
 
+    }
 }
