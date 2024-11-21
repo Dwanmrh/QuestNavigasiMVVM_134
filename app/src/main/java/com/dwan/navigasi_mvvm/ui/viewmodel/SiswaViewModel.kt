adf3820df58ce854b618cfac6935f9fa6b32a364
RidwanC134 <ridwanhh10@gmail.com>
2024-11-21 23:06:34 +0700

    Membuat variabel statusUI yang dapat diakses dari luar, mengubah MutableStateFlow menjadi StateFlow yang tidak bisa dimodifikasi.

--- app/src/main/java/com/dwan/navigasi_mvvm/ui/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/dwan/navigasi_mvvm/ui/viewmodel/SiswaViewModel.kt
@@ -1,7 +1,16 @@
 package com.dwan.navigasi_mvvm.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.dwan.navigasi_mvvm.model.DataSiswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class SiswaViewModel : ViewModel {
 
+    // Request
+    private val _statusUI = MutableStateFlow(DataSiswa())
+
+    // Response
+    val statusUI: StateFlow<DataSiswa> = _statusUI.asStateFlow()
 }
