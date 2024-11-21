1358a8f4fc83f252f6886436f25537b86b9c369b
RidwanC134 <ridwanhh10@gmail.com>
2024-11-21 22:51:27 +0700

    Mendefinisikan fungsi TampilDataView untuk menampilkan data dengan parameter untuk menyesuaikan layout.

--- app/src/main/java/com/dwan/navigasi_mvvm/ui/view/TampilDataView.kt
+++ app/src/main/java/com/dwan/navigasi_mvvm/ui/view/TampilDataView.kt
@@ -1,10 +1,14 @@
 package com.dwan.navigasi_mvvm.ui.view
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.dwan.navigasi_mvvm.model.DataSiswa
 
 @Composable
 fun TampilDataView(
-
+    modifier: Modifier,
+    uiState: DataSiswa,
+    onBackButton: () -> Unit
 ) {
 
 }
